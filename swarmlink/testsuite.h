b0d6a74b810cdb2fad6db5cbb08b68eeebdcc54f
nnhw <nnhw667@gmail.com>
2018-11-29 18:57:48 +0300

    ardupilotmega dependencies added

--- swarmlink/testsuite.h
+++ swarmlink/testsuite.h
@@ -13,16 +13,25 @@ extern "C" {
 #ifndef MAVLINK_TEST_ALL
 #define MAVLINK_TEST_ALL
 static void mavlink_test_ardupilotmega(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_common(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_uAvionix(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_icarous(uint8_t, uint8_t, mavlink_message_t *last_msg);
 static void mavlink_test_swarmlink(uint8_t, uint8_t, mavlink_message_t *last_msg);
 
 static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_ardupilotmega(system_id, component_id, last_msg);
+    mavlink_test_common(system_id, component_id, last_msg);
+    mavlink_test_uAvionix(system_id, component_id, last_msg);
+    mavlink_test_icarous(system_id, component_id, last_msg);
     mavlink_test_swarmlink(system_id, component_id, last_msg);
 }
 #endif
 
 #include "../ardupilotmega/testsuite.h"
+#include "../common/testsuite.h"
+#include "../uAvionix/testsuite.h"
+#include "../icarous/testsuite.h"
 
 
 static void mavlink_test_swarm_beat(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
